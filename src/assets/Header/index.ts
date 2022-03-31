7d95d9fddbba643f6ed8ed11eb00ebffba2dd105
justinlee05 <justinlee05@naver.com>
2022-03-31 19:23:51 +0900

    [ADD] Header 이미지 추가

---
+++ src/assets/Header/index.ts
@@ -0,0 +1,4 @@
+export { default as logo_img } from "./logo.svg";
+export { default as defprofile_img } from "./profile_def.svg";
+export { default as alarm_img } from "./alarm.svg";
+export { default as bookmark_img } from "./bookmark.svg";

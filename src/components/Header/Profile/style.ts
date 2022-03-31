6b90ef8b517746044b2fb4239e4f44ed5f00aca1
justinlee05 <justinlee05@naver.com>
2022-03-31 19:24:33 +0900

    [ADD] Header view

---
+++ src/components/Header/Profile/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const S = {
+  TotalWrapper: styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  `,
+  ProfileImgWrapper: styled.div`
+    width: 40px;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  ProfileName: styled.p`
+    width: 70px;
+    font-size: 21px;
+    margin-left: 20px;
+  `,
+};

de28a6a45bd5b3d5bf86f051f7f48c318d70e685
justinlee05 <justinlee05@naver.com>
2022-03-31 19:28:01 +0900

    [ADD] Globalstyle

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    *{
+        outline:none;
+        border:none;
+        padding:0px;
+        margin:0px;
+        box-sizing:border-box;
+        -webkit-user-select:none; 
+        -moz-user-select:none; 
+        -ms-user-select:none; 
+        user-select:none;
+        img{
+            -webkit-user-drag: none;
+        }
+        
+    }
+    #RouterWrapper{
+        width:100%;
+        min-height:calc(100vh - 80px);
+        
+        max-height:fit-content;
+        margin-top:80px;
+        overflow-x:hidden;
+    }
+`;

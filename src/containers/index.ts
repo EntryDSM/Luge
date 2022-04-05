9247b52d7eca468d944cf2b2f16fa7e1808314a6
justinlee05 <justinlee05@naver.com>
2022-04-05 18:53:18 +0900

    [ADD] 컴포넌트 컨테이너화
    Notice-container

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,3 +1,4 @@
 export {default as HeaderContainer} from './Header';
 export {default as MainContainer} from './Main';
 export {default as LoadingContainer} from './Loading';
+export {default as NoticeContainer} from './Notice'
